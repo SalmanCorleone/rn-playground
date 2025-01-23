82d312b6b2175b8a92d07a3bd8e378aad8582577
salman <samilsalman12@gmail.com>
2025-01-23 11:47:59 +0600

    feat: add expo router

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+interface IProps {}
+
+function Home(props: IProps) {
+  return (
+    <View>
+      <Text>Hello home</Text>
+    </View>
+  );
+}
+
+export default Home;
